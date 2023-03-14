729eb2c20703ddac6bf386747b4481047cfbe352
Wang Shao-Lei <84257391+solomon12354@users.noreply.github.com>
2023-03-14 11:27:43 +0800

    Update HW4.c

--- HW4.c
+++ HW4.c
@@ -1,39 +1,30 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+static int arr[8] = {1000,500,200,100,50,10,5,1};
+
 int numberOfCoin(int value){
+
+
+
     if(value == 0){
         return 0;
     }
-    if(value == 1 || value == 5 || value == 10 || value == 50 || value == 100 || value == 200 || value == 500 || value == 1000){
-        return 1;
-    }else{
 
-        if(value > 1000){
-            return 1 + numberOfCoin(value - 1000);
-        }
-        if(value > 500){
-            return 1 + numberOfCoin(value - 500);
-        }
-        if(value > 200){
-            return 1 + numberOfCoin(value - 200);
-        }
-        if(value > 100){
-            return 1 + numberOfCoin(value - 100);
-        }
-        if(value > 50){
-            return 1 + numberOfCoin(value - 50);
-        }
-        if(value > 10){
-            return 1 + numberOfCoin(value - 10);
-        }
-        if(value > 5){
-            return 1 + numberOfCoin(value - 5);
-        }
-        if(value > 1){
-            return 1 + numberOfCoin(value - 1);
+    int c;
+
+    for(c=0;c<8;c++){
+        if(value == arr[c]){
+            return 1;
         }
     }
+
+    for(c=0;c<8;c++){
+        if(value > arr[c]){
+            return 1 + numberOfCoin(value - arr[c]);
+        }
+    }
+
     return 0;
 }
 
